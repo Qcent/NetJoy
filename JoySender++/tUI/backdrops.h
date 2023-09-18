b2c8351d1ecd23ec946a0d2c3db58cb9aea0e898
Qcent <dquinn8@cogeco.ca>
2023-09-17 23:29:45 -0400

    added copyright / licence to documents

--- JoySender++/tUI/backdrops.h
+++ JoySender++/tUI/backdrops.h
@@ -1,4 +1,27 @@
-﻿#pragma once
+﻿/*
+
+Copyright (c) 2023 Dave Quinn <qcent@yahoo.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+#pragma once
 
 const wchar_t* JoySendMain_Backdrop = LR"(     ,_____________________________________________________________,    
     /_______________________________________________________________\    
@@ -52,15 +75,15 @@ const wchar_t* DS4_Backdrop = LR"(
                ,/______\_     Mode: 2 (DS4)     _/______\,               
               /\________/#########(-Q-)#########\________/\              
              @                                             @             
-            @      __           X: 138.16            ..     @            
-           @    __|  |__   ( )  Y:-218.47  ( )      |  |     @           
-          //   |__    __|       Z: 112.62        ..  `` ..   \\          
+            @      __                                ..     @            
+           @    __|  |__   ( )             ( )      |  |     @           
+          //   |__    __|                        ..  `` ..   \\          
          /@       |__|                          |  |   |  |   @\         
          @'               .--.             .--.  ``  .. ``    '@         
         |/               {    }    [_]    {    }    |  |       \|        
         @                 '--'  /^^^^^^^\  '--'      ``         @        
         @              @'""""""'         '""""""'@              @        
-        @             @'    (M) Toggle Motion    '@             @        
+        @             @'                         '@             @        
         @            @'                           '@            @        
          @.        .@'                             '@         ,@         
           `@@@@@@@@'                                 '@@@@@@@@`          
