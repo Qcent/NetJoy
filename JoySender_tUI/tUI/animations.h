17e611e7323b0404c0840f7fb2825fb8f3dfa352
Qcent <dquinn8@cogeco.ca>
2024-08-31 00:44:13 -0400

    confimed bad optimization, noinlined countUpDown

--- JoySender_tUI/tUI/animations.h
+++ JoySender_tUI/tUI/animations.h
@@ -68,7 +68,7 @@ const wchar_t* FooterAnimation[CX_ANI_FRAME_COUNT + 1] = {
 
 // function will inc/dec &counter up to maxCount and down to 0
 // for animating back and forth through frames
-//__declspec(noinline) // gets optimized out in Release x64?
+__declspec(noinline) // gets optimized out in Release x64? yes!
 void countUpDown(int& counter, int maxCount) { // * a counter value outside of (0 - maxCount) will cause issues
     static int dir = 1; // 1: up, -1: down
 
