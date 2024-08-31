2f607ff1f797ce88a63c1c08ab4359518d578893
Qcent <dquinn8@cogeco.ca>
2024-08-30 22:57:59 -0400

    added tUI exclusive console struct to textUI.h, cutting down on duplicate code

--- JoySender_tUI/tUI/textUI.h
+++ JoySender_tUI/tUI/textUI.h
@@ -8,3 +8,35 @@
 #include "backdrops.h"
 #include "uiButtons.h"
 #include "controllerFaces.h"
+
+//----------------------------------------------------------------------------
+// lifted from : https://cplusplus.com/forum/windows/10731/
+struct console
+{
+    console(unsigned width, unsigned height)
+    {
+        SMALL_RECT r;
+        COORD      c;
+        hConOut = GetStdHandle(STD_OUTPUT_HANDLE);
+        GetConsoleScreenBufferInfo(hConOut, &csbi);
+
+        r.Left = r.Top = 0;
+        r.Right = width;
+        r.Bottom = height + 1;
+        SetConsoleWindowInfo(hConOut, TRUE, &r);
+
+        c.X = width;
+        c.Y = height;
+        SetConsoleScreenBufferSize(hConOut, c);
+    }
+
+    ~console()
+    {
+        SetConsoleTextAttribute(hConOut, csbi.wAttributes);
+        SetConsoleScreenBufferSize(hConOut, csbi.dwSize);
+        SetConsoleWindowInfo(hConOut, TRUE, &csbi.srWindow);
+    }
+
+    HANDLE                     hConOut;
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+};
